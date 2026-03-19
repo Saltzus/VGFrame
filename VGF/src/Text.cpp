03c5cb39bb23da531d0f3f6fbc33e921e236f5ab
saltzus <sasu.lahtinen@outlook.com>
2026-03-19 20:56:31 +0200

    Start for text system (not working)

--- VGF/src/Text.cpp
+++ VGF/src/Text.cpp
@@ -4,50 +4,57 @@ namespace VGF
 {
     Text::Text()
     {
-        //FT_Library ft;
-        //if (FT_Init_FreeType(&ft))
-        //{
-        //    std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
-        //}
-        //
-        //FT_Face face;
-        //if (FT_New_Face(ft, "Game/Resources/Fonts/Testi.ttf", 0, &face))
-        //{
-        //    std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
-        //}
-        //
-        //FT_Set_Pixel_Sizes(face, 0, 48);  
-        //if (FT_Load_Char(face, 'X', FT_LOAD_RENDER))
-        //{
-        //    std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;  
-        //}
-        //
-        //for (unsigned char c = 0; c < 128; c++)
-        //{
-        //    // load character glyph 
-        //    if (FT_Load_Char(face, c, FT_LOAD_RENDER))
-        //    {
-        //        std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
-        //        continue;
-        //    }
-        //
-        //    // generate texture
-        //    Texture texture(face->glyph->bitmap.buffer, 1, face->glyph->bitmap.width, face->glyph->bitmap.rows);
-        //    // now store character for later use
-        //    Character character = {
-        //        texture, 
-        //        glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
-        //        glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
-        //        face->glyph->advance.x
-        //    };
-        //    Characters.insert(std::pair<char, Character>(c, character));
-        //}
-        //
-        //FT_Done_Face(face);
-        //FT_Done_FreeType(ft);
+        if (FT_Init_FreeType(&ft))
+        {
+            std::cout << "[ERROR] Could not init FreeType Library" << std::endl;
+        }
+
+        FT_Face face;
+        if (FT_New_Face(ft, "Game/Resources/Fonts/Testi.ttf", 0, &face))
+        {
+            std::cout << "[ERROR] Failed to load font" << std::endl;
+        }
+
+        FT_Set_Pixel_Sizes(face, 0, 48);
+        if (FT_Load_Char(face, 'X', FT_LOAD_RENDER))
+        {
+            std::cout << "[ERROR] Failed to load Glyph" << std::endl;
+        }
+
+        for (unsigned char c = 0; c < 128; c++)
+        {
+            // load character glyph
+            if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+            {
+                std::cout << "[ERROR] Failed to load Glyph" << std::endl;
+                continue;
+            }
+
+            // generate texture
+            texture = new Texture(face->glyph->bitmap.buffer, 1, face->glyph->bitmap.width, face->glyph->bitmap.rows);
+            renderer = new Renderer(quadIndices,quadVertices, {MatrixBufferObject::getDefault()});
+
+            MatrixData data;
+            data.model = glm::mat4(1.f);
+            data.proj = camera->projection;
+            data.view = camera->view;
+            Model::matrixBuffer.SetData((void*)&data);
+
+            debug->indices.clear();
+            debug->vertices.clear();
+
+            dynamicsWorld->debugDrawWorld();
+
+            lines = new VGF::Renderer(debug->indices, debug->vertices, );
+            lines->Render(config, { &matrixBuffer });
+        }
+
+        FT_Done_Face(face);
+        FT_Done_FreeType(ft);
     }
     
     Text::~Text()
     {
+        delete texture;
     }
 }
--- VGF/src/Text.h
+++ VGF/src/Text.h
@@ -1,13 +1,13 @@
 #pragma once
 
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
 #include <glm/glm.hpp>
 #include <map>
 
+#include "Model.h"
 #include "Texture.h"
-
-#include <iostream>
-//#include <ft2build.h>
-//#include FT_FREETYPE_H  
 
 namespace VGF
 {
@@ -18,15 +18,25 @@ namespace VGF
         Text();
         ~Text();
     private:
-        //FT_Library ft;
-    };
-    
-    struct Character {
-        //Texture TextureID;  // ID handle of the glyph texture
-        glm::ivec2   Size;       // Size of glyph
-        glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
-        //FT_Pos Advance;    // Offset to advance to next glyph
+        Texture* texture;
+        Renderer* renderer;
+
+        FT_Library ft;
+
+        const static inline std::vector<float> quadVertices =
+        {
+            // BL (x, y, z)               (nx, ny, nz)        (r,g,b)             (u, v)
+            -0.5000f, 0, -0.5000f,   0.0f, -1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, // 0
+             0.5000f, 0, -0.5000f,   0.0f, -1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f, // 1
+             0.5000f, 0,  0.5000f,   0.0f, -1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, // 2
+            -0.5000f, 0,  0.5000f,   0.0f, -1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f  // 3
+         };
+
+        const static inline std::vector<unsigned int> quadIndices =
+        {
+            0, 1, 2,
+            2, 3, 0
+        };
     };
 
-    std::map<char, Character> Characters;
 } 
